d13c1f3b631fbbc490ca629c5852aff66a1823ef
Adam Nalgiev <adamnalgiev006@gmail.com>
2025-02-12 18:52:04 +0300

    Added HTTP logging interceptor

--- RMUniverse/app/src/main/java/com/na/rmuniverse/data/network/Client.kt
+++ RMUniverse/app/src/main/java/com/na/rmuniverse/data/network/Client.kt
@@ -1,14 +1,20 @@
 package com.na.rmuniverse.data.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Inject
 
-class Client @Inject constructor(){
+class Client @Inject constructor() {
+
+    private val logging = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    private val httpClient = OkHttpClient.Builder().addInterceptor(logging).build()
 
     val requests: Api = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(httpClient)
         .build()
         .create(Api::class.java)
 
